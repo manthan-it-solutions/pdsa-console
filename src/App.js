68cb4847b8c150fab8321a19bd2e49ed8e0a4d0a
Ramkesh Nishad <ramkesh@manthanitsolutions.in>
2024-11-25 11:32:29 +0530

    app setup

--- src/App.js
+++ src/App.js
@@ -2,40 +2,22 @@ import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, useLocation, useNavigate, Navigate } from 'react-router-dom';
 import Header from './components/header/Header';
 import Footer from './components/footer/footer';
-import WbQuickCampaigns from './pages/WbQuickCampaigns';
-import WbCustomizeCampaigns from './pages/WbCustomizeCampaigns';
-import WbCampaignsbutton from './pages/WbCompaignButton'
-import WbTemplate from './pages/NationalCompaign';
-import WbManageMedia from './pages/InternationalCompaign';
-import ButtonCompaign from './pages/ButtonCampaign';
-import ButtonIdCompaign from './pages/ButtonCampaignId'
-import InternationalCompaign from './pages/InternationalCompaignId'
-import PandingCampaign from './pages/admin/pandingCampaignView'
-import WbManageDealer from './pages/admin/WbManageDealer';
-import WbTransactionDetail from './pages/admin/WbTransactionDetail';
-import Dashboard from './pages/Dashboard';
-import AdminDashboard from './pages/admin/adminDashboard';
-import AdminGuard from './guards/AdminGuard';
-// import Drag from './pages/Drag'
+
 import WbLoginDetails from './pages/WbLoginDetails';
-import Demo from './pages/Demo';
+import Dashboard from './pages/Dashboard.jsx'
 import Login from './pages/Login';
 import { useUIContext } from './context';
 import AuthGuard from './guards/authGuards';
 import { apiCall, Me } from './services/authServieces';
 
-import Managebot from './pages/Managebot';
+import AdminGuard from './guards/AdminGuard';
 
 import Profile from './pages/profile';
 import Changepassword from './pages/Changepassword';
-import ManageWaba from './pages/admin/ManageWaba'
-import NationCompaignId from "../src/pages/NationalCompaignId"
-import NationalReport  from './pages/NationalReport'
-import Internationalreport from './pages/Internationalreport'
-import ButtonReport  from './pages/ButtonReport'
+
 
 import CompeleteCampaign from './pages/admin/CompleteCompaign.jsx'
-import TrsansactionDetails from './pages/TransactionDetails.jsx'
+
 import ZoneReprot from '../src/pages/admin/zonewise_report.jsx'
 import ZoneWise_Report_User from './pages/zonewise_report_user.jsx'
 
@@ -47,6 +29,8 @@ import DealerDetailsPage from './pages/admin/show_data_zone_wise.jsx'
 import DealerDetailsPageregion from './pages/admin/show_data_region_wise.jsx'
 
 import NotFound from './pages/404_page.jsx'
+import AdminDashboard from './pages/admin/adminDashboard';
+import WbManageDealer from './pages/admin/WbManageDealer';
 
 
 
@@ -141,23 +125,14 @@ const Layout = () => {
             {/* Protected Routes */}
             {/* <Route path="/drag" element={<Drag />} /> */}
             <Route path="/user_dashbaord" element={<AuthGuard><Dashboard /></AuthGuard>} />
-            <Route path="/wbquickcampaigns" element={<AuthGuard><WbQuickCampaigns /></AuthGuard>} />
-            <Route path="/wbcustomizecampaigns" element={<AuthGuard><WbCustomizeCampaigns /></AuthGuard>} />
-            <Route path="/NationCompaignId" element={<AuthGuard><NationCompaignId /></AuthGuard>} />
-            
-            <Route path="/wbcampaignsbutton" element={<AuthGuard><WbCampaignsbutton /></AuthGuard>} />
-            <Route path="/wbtemplate" element={<AuthGuard><WbTemplate /></AuthGuard>} />
-            <Route path="/wbmanagemedia" element={<AuthGuard><WbManageMedia /></AuthGuard>} />
-            <Route path="/internationCompaign" element={<AuthGuard><InternationalCompaign/></AuthGuard>} />
-            <Route path="/wbmanagegroups" element={<AuthGuard><ButtonCompaign /></AuthGuard>} />
-            
-            <Route path="/ButtonCompaignId" element={<AuthGuard><ButtonIdCompaign /></AuthGuard>} />
-           
+
+
+            <Route path="/admin" element={<AuthGuard><AdminGuard><AdminDashboard /></AdminGuard></AuthGuard>} />
+                  
             <Route path="/wblogindetails" element={<AuthGuard><WbLoginDetails /></AuthGuard>} />
-            <Route path="/demo" element={<AuthGuard><Demo /></AuthGuard>} />
+      
 
-
-            <Route path="/TransactionDetails" element={<AuthGuard><TrsansactionDetails /></AuthGuard>} />
+            
             <Route path="/ZoneWiseReport" element={<AuthGuard><ZoneReprot /></AuthGuard>} />
 
 
@@ -166,7 +141,7 @@ const Layout = () => {
 
             <Route path="/RegioneWise_report_user" element={<AuthGuard><RegionWise_report_user /></AuthGuard>} />
 
-
+            <Route path="/admin/manage-dealer" element={<AuthGuard><AdminGuard><WbManageDealer /></AdminGuard></AuthGuard>} />
             <Route path="/DealerDetailsPage" element={<AuthGuard><DealerDetailsPage /></AuthGuard>} />
 
 
@@ -178,43 +153,14 @@ const Layout = () => {
             {/* report routes   start  */}
 
 
-            <Route path="/NationalReport" element={<AuthGuard><NationalReport /></AuthGuard>} />
-            <Route path="/InterNationalReport" element={<AuthGuard><Internationalreport /></AuthGuard>} />
-            <Route path="/ButttonReport" element={<AuthGuard><ButtonReport /></AuthGuard>} />
+          
             <Route path="/CompleteCampaign" element={<AuthGuard><CompeleteCampaign /></AuthGuard>} />
 
             
-            
-{/*   Admin Routes of Admin Dashbaordv Start */}
-
-<Route path="/admin/panding_Campaign" element={<AuthGuard><AdminGuard><PandingCampaign /></AdminGuard></AuthGuard>} />
-
-
-
-
-{/*   Admin Routes of Admin Dashbaord End */}
-
-            
-
-
-
-
-                 {/* report routes   End */}
 
 
             {/* Admin Routes*/}
-            <Route path="/admin" element={<AuthGuard><AdminGuard><AdminDashboard /></AdminGuard></AuthGuard>} />
-            <Route path="/admin/transaction-details" element={<AuthGuard><AdminGuard><WbTransactionDetail /></AdminGuard></AuthGuard>} />
-            <Route path="/admin/manage-dealer" element={<AuthGuard><AdminGuard><WbManageDealer /></AdminGuard></AuthGuard>} />
-            <Route path="/admin/manage-waba" element={<AuthGuard><AdminGuard><ManageWaba/></AdminGuard></AuthGuard>} />
-
-            <Route path="/admin/manage-quikmassage" element={<AuthGuard><AdminGuard><ManageWaba/></AdminGuard></AuthGuard>} />
-            {/* <Route path="/admin/manage-waba" element={<AuthGuard></AuthGuard>} /> */}
-
-
-       
-            <Route path="/managebot" element={<Managebot />} />
-
+  
             <Route path="/profile" element={<AuthGuard><Profile/></AuthGuard>} />
             <Route path="/changepassword" element={<AuthGuard><Changepassword/></AuthGuard>} />
             
